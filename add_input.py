c4fee422e537d7bb6a376ebce1a3a9b82610e7fd
dudkaa85 <143198905+dudkaa85@users.noreply.github.com>
2024-12-07 06:38:38 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,9 @@
+name = input("ведите ваше имя")
+print(name)
+yourLastName = input("ведите вашу фамилию")
+print(yourLastName)
+fatherSName = input("отчество")
+print(fatherSName )
+greetings = "Здравствуйте,HI",yourLastName,name,fatherSName
+print( greetings)
+  #этап 1 задание 3
