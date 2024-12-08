12cfb37bea1941afda893c70abbe8ffc5eb0313e
dudkaa85 <143198905+dudkaa85@users.noreply.github.com>
2024-12-08 03:53:56 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,2 +1,11 @@
-date = input('ведите дату:') # этап 1 задание 2
-print(date)
+# date = input('ведите дату:') # этап 1 задание 2
+# print(date)
+
+
+import datetime
+
+now = datetime.datetime.now()
+print(now)#текщая дата
+# дата создание обьекта
+birthday = datetime.date(2024,12, 7)
+print(birthday)
