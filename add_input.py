e11c9291b3160d538ec6aba2e482b4414a46cb48
dudkaa85 <143198905+dudkaa85@users.noreply.github.com>
2024-12-15 07:46:05 +0300

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,12 +1,3 @@
-# name = input("ведите ваше имя")
-# print(name)
-# yourLastName = input("ведите вашу фамилию")
-# print(yourLastName)
-# fatherSName = input("отчество")
-# print(fatherSName )
-# greetings = "Здравствуйте,HI",yourLastName,name,fatherSName
-# print( greetings)
-#   #этап 1 задание 3
 
 # Запрашиваем у пользователя информацию
 username = input("Введите имя пользователя: ")
